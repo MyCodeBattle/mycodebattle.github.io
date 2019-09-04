cddc830f3cf2529dcabacc62ea2ae44e05407199
PureFrog <mycodebattle@outlook.com>
2019-09-05 00:08:22 +0800

    update

--- new.py
+++ new.py
@@ -5,7 +5,7 @@ current_time = time.strftime('%Y-%m-%d-', time.localtime())
 filename = current_time + sys.argv[1] + '.md'
 f = open('./_posts/' + filename, 'w')
 
-content = '---\ntitle: \nlayout: post\ndate: ' + current_time[:-1] + '\ncategories: \n---\n'
+content = '---\ntitle: {}\nlayout: post\ndate: '.format(sys.argv[1]) + current_time[:-1] + '\ncategories: \n---\n'
 f.write(content)
 f.close()
 
